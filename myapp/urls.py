b617bfbc12918094dedf24f3327ce3da5aece700
krr006 <desper1609@gmail.com>
2023-09-25 23:18:49 +0300

    add catalog list

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('catalog/', views.catalog, name='catalog'),
 ]
+# if settings.DEBUG:
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
