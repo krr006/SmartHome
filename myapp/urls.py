5b560632119ff539646a5305837628ee7b4e9dd4
krr006 <desper1609@gmail.com>
2023-09-30 20:43:57 +0300

    Add FAQ page

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
     path('catalog/', views.catalog, name='catalog'),
+    path('faq', views.faq, name='faq'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
 # if settings.DEBUG:
--- myapp/views.py
+++ myapp/views.py
@@ -14,6 +14,9 @@ def catalog(request):
     products = Product.objects.all()
     return render(request, 'myapp/catalog.html', {'products': products})
 
+def faq(request):
+    return render(request, 'myapp/faq.html')
+
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'myapp/product_detail.html', {'product': product})
